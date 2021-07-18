9acdd619c0d483782e7db4d53e153597421d8687
alibinsaleh <alibinsaleh@gmail.com>
2021-07-18 06:29:29 +0300

    dabbling with templates and bootstrap

--- grades/urls.py
+++ grades/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from grades import views
 
 urlpatterns = [
-	path('', views.home),
+	path('', views.home, name='home'),
+	path('<int:year>/<str:month>/', views.home, name='home'),
 ]
--- grades/views.py
+++ grades/views.py
@@ -1,7 +1,59 @@
 from django.shortcuts import render
 from .models import Student
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
+
 # Create your views here.
 
-def home(request):
-	students = Student.objects.all()
-	return render(request, 'home.html', context={'students':students})
+def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
+	month = month.capitalize()
+	# Convert month from name to number
+	month_number = list(calendar.month_name).index(month)
+	month_number = int(month_number)
+	# Create a calendar
+	cal = HTMLCalendar().formatmonth(year, month_number)
+	
+	# Get current year
+	now = datetime.now()
+	current_year = now.year
+
+	# Get current time
+	time = now.strftime('%I:%M:%S %p')
+
+	return render(request, 'events/home.html', 
+			context= {
+				'year': year,
+				'month': month,
+				'month_number': month_number,
+				'cal': cal,
+				'current_year': current_year,
+				'time': time,
+			}
+		)
+
+def show_calendar(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
+	month = month.capitalize()
+	# Convert month from name to number
+	month_number = list(calendar.month_name).index(month)
+	month_number = int(month_number)
+	# Create a calendar
+	cal = HTMLCalendar().formatmonth(year, month_number)
+	
+	# Get current year
+	now = datetime.now()
+	current_year = now.year
+
+	# Get current time
+	time = now.strftime('%I:%M:%S %p')
+
+	return render(request, 'events/calendar.html', 
+			context= {
+				'year': year,
+				'month': month,
+				'month_number': month_number,
+				'cal': cal,
+				'current_year': current_year,
+				'time': time,
+			}
+		)
