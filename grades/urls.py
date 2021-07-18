9be2c8c1922c058236f4804d4d5175d649942430
alibinsaleh <alibinsaleh@gmail.com>
2021-07-18 13:12:07 +0300

    dabbling with templates and bootstrap2

--- grades/urls.py
+++ grades/urls.py
@@ -4,4 +4,5 @@ from grades import views
 urlpatterns = [
 	path('', views.home, name='home'),
 	path('<int:year>/<str:month>/', views.home, name='home'),
+	path('student/<int:student_id>/', views.student_details, name='student'),
 ]
--- grades/views.py
+++ grades/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Student
 import calendar
 from calendar import HTMLCalendar
@@ -7,6 +7,9 @@ from datetime import datetime
 # Create your views here.
 
 def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
+	# Get names of students
+	students = Student.objects.all()
+
 	month = month.capitalize()
 	# Convert month from name to number
 	month_number = list(calendar.month_name).index(month)
@@ -29,31 +32,16 @@ def home(request, year=datetime.now().year, month=datetime.now().strftime('%B'))
 				'cal': cal,
 				'current_year': current_year,
 				'time': time,
+				'students': students,
 			}
 		)
 
-def show_calendar(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
-	month = month.capitalize()
-	# Convert month from name to number
-	month_number = list(calendar.month_name).index(month)
-	month_number = int(month_number)
-	# Create a calendar
-	cal = HTMLCalendar().formatmonth(year, month_number)
-	
-	# Get current year
-	now = datetime.now()
-	current_year = now.year
+def student_details(request, student_id=1):
+	student = get_object_or_404(Student, pk=student_id)
+	#student = Student.objects.find(student_id)
 
-	# Get current time
-	time = now.strftime('%I:%M:%S %p')
-
-	return render(request, 'events/calendar.html', 
+	return render(request, 'students/student_details.html', 
 			context= {
-				'year': year,
-				'month': month,
-				'month_number': month_number,
-				'cal': cal,
-				'current_year': current_year,
-				'time': time,
+				'student': student,
 			}
 		)
